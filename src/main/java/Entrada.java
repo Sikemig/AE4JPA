95b2d7a38977fd5500409094786f76104dd16b6f
Sikemig <sikemig@gmail.com>
2025-02-01 13:55:26 +0100

    final

--- src/main/java/Entrada.java
+++ src/main/java/Entrada.java
@@ -12,32 +12,34 @@ public class Entrada {
         LibrosController librosController = new LibrosController();
 
         // Añadir 3 autores
-        //librosController.crearAutor(new Autor("Sikem", "Iglesias", "20-06-1990"));
-        //librosController.crearAutor(new Autor("Alicia", "Lopez", "01-01-1980"));
-        //librosController.crearAutor(new Autor("Borja", "Martin"));
-
+        /*librosController.crearAutor(new Autor("SikemNUEVO", "Iglesias", "20-06-1990"));
+        librosController.crearAutor(new Autor("AliciaNUEVO", "Lopez", "01-01-1980"));
+        librosController.crearAutor(new Autor("BorjaNUEVO", "Martin"));
+        */
 
         // Añadir 2 editoriales
-        //librosController.crearEditorial(new Editorial("Planeta", "C/ Falsa, 123"));
-        //librosController.crearEditorial(new Editorial("Alma", "C/ Verdadera, 987"));
+        //librosController.crearEditorial(new Editorial("PlanetaNUEVO", "C/ Falsa, 123"));
+        //librosController.crearEditorial(new Editorial("AlmaNUEVO", "C/ Verdadera, 987"));
+
 
 
         // Añadir 8 libros
-        //librosController.crearLibro(new Libro("El Quijote", 19.99), 1, 1);
-        //librosController.crearLibro(new Libro("El Señor de los Anillos", 29.99), 1, 1);
-        //librosController.crearLibro(new Libro("Orgullo y Prejuicio", 15.99), 2, 1);
-        //librosController.crearLibro(new Libro("Cronica de una Muerte Anunciada", 20.00), 3, 1);
-        //librosController.crearLibro(new Libro("Muerte en el Nilo", 19.99), 2, 2);
-        //librosController.crearLibro(new Libro("Asesinato en el Orient Express", 19.99), 2, 2);
-        //librosController.crearLibro(new Libro("Ready Player One", 19.99), 3, 2);
-        //librosController.crearLibro(new Libro("Ready Player Two", 19.99), 3, 2);
+        /*librosController.crearLibro(new Libro("El QuijoteNUEVO", 19.99), 4, 3);
+        librosController.crearLibro(new Libro("El Señor de los AnillosNUEVO", 29.99), 4, 3);
+        librosController.crearLibro(new Libro("Orgullo y PrejuicioNUEVO", 15.99), 5, 3);
+        librosController.crearLibro(new Libro("Cronica de una Muerte AnunciadaNUEVO", 20.00), 6, 3);
+        librosController.crearLibro(new Libro("Muerte en el NiloNUEVO", 19.99), 5, 4);
+        librosController.crearLibro(new Libro("Asesinato en el Orient ExpressNUEVO", 19.99), 5, 4);
+        librosController.crearLibro(new Libro("Ready Player OneNUEVO", 19.99), 6, 4);
+        librosController.crearLibro(new Libro("Ready Player TwoNUEVO", 19.99), 6, 4);
+        */
 
         // Añadir 2 librerias
-        //librosController.crearLibreria(new Libreria("Re-read", "Pepe", "Avenida Aleatoria, 555"));
-        //librosController.crearLibreria(new Libreria("Cervantes", "Paco", "Plaza Fija, 2"));
+        //librosController.crearLibreria(new Libreria("Re-readNUEVO", "Pepe", "Avenida Aleatoria, 555"));
+        //librosController.crearLibreria(new Libreria("CervantesNUEVO", "Paco", "Plaza Fija, 2"));
 
         // Asociar 4 libros a cada libreria
-        //librosController.asociarLibros(1,1);
+        /*librosController.asociarLibros(1,1);
         //librosController.asociarLibros(3,1);
         //librosController.asociarLibros(5,1);
         //librosController.asociarLibros(7,1);
@@ -45,15 +47,15 @@ public class Entrada {
         //librosController.asociarLibros(4,2);
         //librosController.asociarLibros(6,2);
         //librosController.asociarLibros(8,2);
-
-
+        */
+        
 
         //Mostrar todos los libros dados de alta, con su editorial y su autor
         //librosController.obtenerLibros();
 
 
         //Mostrar todos los autores dados de alta, con sus libros asociados
-        librosController.obtenerAutores();
+        //librosController.obtenerAutores();
 
 
         ///Mostrar todas las librerías, con solamente sus libros asociados
